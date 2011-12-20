a13769c12b4b7b0a3923a34b6216578668ba6c6c
hayashikejinan <hayashikejinan@gmail.com>
2011-12-21 00:58:18 +0900

    bluetooth: clean up debugging and error messages
    Change from printk to dev_ logging.
    Remove debug messages during module init and exit.
    
    Change-Id: I7628b076d94791daca13ff6c98dbfa11bc51c546
    Signed-off-by: Matthew Wilson <mtwilson@codeaurora.org>

--- kernel/drivers/bluetooth/bluetooth-power.c
+++ kernel/drivers/bluetooth/bluetooth-power.c
@@ -93,8 +93,7 @@ static int bluetooth_power_rfkill_probe(struct platform_device *pdev)
 	rfkill = rfkill_allocate(&pdev->dev, RFKILL_TYPE_BLUETOOTH);
 
 	if (!rfkill) {
-		printk(KERN_DEBUG
-			"%s: rfkill allocate failed\n", __func__);
+		dev_err(&pdev->dev, "rfkill allocate failed\n");
 		return -ENOMEM;
 	}
 
@@ -103,9 +102,7 @@ static int bluetooth_power_rfkill_probe(struct platform_device *pdev)
 	rfkill->data = pdev->dev.platform_data;
 	ret = rfkill_register(rfkill);
 	if (ret) {
-		printk(KERN_DEBUG
-			"%s: rfkill register failed=%d\n", __func__,
-			ret);
+		dev_err(&pdev->dev, "rfkill register failed=%d\n", ret);
 		rfkill_free(rfkill);
 		return ret;
 	}
@@ -119,6 +116,8 @@ static void bluetooth_power_rfkill_remove(struct platform_device *pdev)
 {
 	struct rfkill *rfkill;
 
+	dev_dbg(&pdev->dev, "%s\n", __func__);
+
 	rfkill = platform_get_drvdata(pdev);
 	if (rfkill)
 		rfkill_unregister(rfkill);
@@ -130,11 +129,10 @@ static int __init bt_power_probe(struct platform_device *pdev)
 {
 	int ret = 0;
 
-	printk(KERN_DEBUG "%s\n", __func__);
+	dev_dbg(&pdev->dev, "%s\n", __func__);
 
 	if (!pdev->dev.platform_data) {
-		printk(KERN_ERR "%s: platform data not initialized\n",
-				__func__);
+		dev_err(&pdev->dev, "platform data not initialized\n");
 		return -ENOSYS;
 	}
 
@@ -145,7 +143,7 @@ static int __init bt_power_probe(struct platform_device *pdev)
 
 static int __devexit bt_power_remove(struct platform_device *pdev)
 {
-	printk(KERN_DEBUG "%s\n", __func__);
+	dev_dbg(&pdev->dev, "%s\n", __func__);
 
 	bluetooth_power_rfkill_remove(pdev);
 
@@ -165,20 +163,18 @@ static int __init bluetooth_power_init(void)
 {
 	int ret;
 
-	printk(KERN_DEBUG "%s\n", __func__);
 	ret = platform_driver_register(&bt_power_driver);
 	return ret;
 }
 
 static void __exit bluetooth_power_exit(void)
 {
-	printk(KERN_DEBUG "%s\n", __func__);
 	platform_driver_unregister(&bt_power_driver);
 }
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("MSM Bluetooth power control driver");
-MODULE_VERSION("1.20");
+MODULE_VERSION("1.40");
 MODULE_PARM_DESC(power, "MSM Bluetooth power switch (bool): 0,1=off,on");
 
 module_init(bluetooth_power_init);
