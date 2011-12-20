9430a1b8f11edabc70861c13d84127a75ff8bc32
hayashikejinan <hayashikejinan@gmail.com>
2011-12-21 01:21:28 +0900

    ARM-specific hooks for physical memory remove.
    Add ARM-specific arch physical remove memory hook.
    
    Change-Id: If935e9aa1547895dd9206dfc7acae07cf0c20978
    Signed-off-by: Larry Bassel <lbassel@codeaurora.org>

--- kernel/include/linux/memory_hotplug.h
+++ kernel/include/linux/memory_hotplug.h
@@ -227,7 +227,6 @@ extern void reserve_hotplug_pages(unsigned long start_pfn,
 extern void unreserve_hotplug_pages(unsigned long start_pfn,
 				unsigned long nr_pages);
 #endif /* __LINUX_MEMORY_HOTPLUG_H */
-extern int physical_remove_memory(u64 start, u64 size);
 extern int arch_physical_remove_memory(u64 start, u64 size);
 extern int physical_low_power_memory(u64 start, u64 size);
 extern int arch_physical_low_power_memory(u64 start, u64 size);
