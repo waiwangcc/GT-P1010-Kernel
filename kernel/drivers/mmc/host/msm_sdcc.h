c28407c23f8f049f6299b4dbe5a6b1da970bc106
hayashikejinan <hayashikejinan@gmail.com>
2011-12-21 00:55:59 +0900

    mmc: msm_sdcc: remove auto suspend code added in 2.6.29 kernel
    The auto-suspend functionality has been removed partially
    during 2.6.32 kernel upgrade. This patch removes it completely.
    
    Change-Id: I23229467946969fbf6841f13676504b6eaad98dd
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- kernel/drivers/mmc/host/msm_sdcc.h
+++ kernel/drivers/mmc/host/msm_sdcc.h
@@ -255,9 +255,6 @@ struct msmsdcc_host {
 
 	struct tasklet_struct 	dma_tlet;
 
-#ifdef CONFIG_MMC_AUTO_SUSPEND
-	unsigned long           suspended;
-#endif
 	unsigned int prog_scan;
 	unsigned int prog_enable;
 
