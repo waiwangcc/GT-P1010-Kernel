90805854ad6c7e7f7d67d60c5f2f95716c30be26
hayashikejinan <hayashikejinan@gmail.com>
2011-12-23 14:56:54 +0900

    drm: Enable drm drivers to add drm sysfs devices.
    Export utility functions for drivers to add specialized devices in the
    sysfs drm class subdirectory.
    
    Initially this will be needed form TTM to add a virtual device that
    handles power management.
    
    Signed-off-by: Thomas Hellstrom <thellstrom@vmware.com>
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- kernel/drivers/gpu/drm/drm_sysfs.c
+++ kernel/drivers/gpu/drm/drm_sysfs.c
@@ -17,6 +17,7 @@
 #include <linux/kdev_t.h>
 #include <linux/err.h>
 
+#include "drm_sysfs.h"
 #include "drm_core.h"
 #include "drmP.h"
 
@@ -534,3 +535,27 @@ void drm_sysfs_device_remove(struct drm_minor *minor)
 		device_remove_file(&minor->kdev, &device_attrs[i]);
 	device_unregister(&minor->kdev);
 }
+
+
+/**
+ * drm_class_device_register - Register a struct device in the drm class.
+ *
+ * @dev: pointer to struct device to register.
+ *
+ * @dev should have all relevant members pre-filled with the exception
+ * of the class member. In particular, the device_type member must
+ * be set.
+ */
+
+int drm_class_device_register(struct device *dev)
+{
+	dev->class = drm_class;
+	return device_register(dev);
+}
+EXPORT_SYMBOL_GPL(drm_class_device_register);
+
+void drm_class_device_unregister(struct device *dev)
+{
+	return device_unregister(dev);
+}
+EXPORT_SYMBOL_GPL(drm_class_device_unregister);
